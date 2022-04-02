4c3a63f77dbd5146fbec39604b9404d7c5fa1793
Osman Yıldırım <yildirim.osman@icloud.com>
2022-04-03 01:35:28 +0300

    Interface Segregation Principle complete

--- SOLID Principles.playground/Pages/Interface Segregation Principle.xcplaygroundpage/Sources/Before.swift
+++ SOLID Principles.playground/Pages/Interface Segregation Principle.xcplaygroundpage/Sources/Before.swift
@@ -1,2 +1,39 @@
 import Foundation
 
+/// Talent protocol for  `SoftwareDeveloper`,` Analyst` etc. classes
+private protocol Talent {
+
+    /// Talent of join meeting
+    func joinMeeting()
+
+    /// Talent of write code
+    func writeCode()
+}
+
+/// Software Developer class with some talents
+private final class SoftwareDeveloper: Talent {
+
+    /// Join meeting method of `Talent` protocol
+    func joinMeeting() {
+        NSLog("Software Developer can join meeting")
+    }
+
+    /// Write code method of `Talent` protocol
+    func writeCode() {
+        NSLog("Software Developer can write code")
+    }
+}
+
+/// Analyst class with some talents
+private final class Analyst: Talent {
+
+    /// Join meeting method of `Talent` protocol
+    func joinMeeting() {
+        NSLog("Analyst can join meeting")
+    }
+
+    /// Write code method of `Talent` protocol
+    func writeCode() {
+        fatalError("Analyst doesn't have the talent write code")
+    }
+}
